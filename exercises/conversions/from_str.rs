a219669a173c65458813a5d7571b50e01eac5f49
彭振 <505380967@qq.com>
2023-10-14 07:19:22 +0000

    feat: finish try_from_into

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,20 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        let p: Vec<&str> = s.split(',').collect();
+        if p.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if p[0].len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+        match p[1].parse::<usize>() {
+            Ok(a) => Ok(Person{ name: p[0].to_string(), age: a}),
+            Err(e) => Err(ParsePersonError::ParseInt(e)),
+        }
     }
 }
 
